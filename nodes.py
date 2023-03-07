83f20405db3664fbffd98fe52be6b8595c39526a
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-03-07 15:59:01 +0100

    Improve Text node and element node classes

--- nodes.py
+++ nodes.py
@@ -1,17 +1,40 @@
-from dataclasses import dataclass
 from enum import Enum
 
-class ElementData:
-    tag_name = str
-    attributes = int
+class Type(Enum):
+    ELEMENT = 1
+    ATTRIBUTE_NODE = 2
+    TEXT = 3
 
-class NodeType(Enum):
-    text = str
-    element = ElementData
-
-@dataclass
 class Node:
-    childern: []
-    node_type: NodeType
+    node_type: Type
+    children: list
+
+    def __init__(self, node_type: Type, children: list):
+        self.node_type = node_type
+        self.children = children
+
+class Text_node(Node):
+    data: str
+
+    def __init__(self, data: str):
+        self.data = data
+        super().__init__(Type.TEXT, [])
+
+class Element_node(Node):
+   tag_name: str
+   attributes: dict[str, str]
+
+   def __init__(self, name: str, attr: dict[str, str]):
+       super().__init__(Type.ELEMENT, [])
+
+       self.tag_name = name
+       self.attributes = attr
+       
+def create_text_node(text: str) -> Node:
+    return Text_node(text)
+
+def create_element_node(name: str, attrs: dict[str, str]) -> Node:
+    return Element_node(name, attrs)
+
 
 
